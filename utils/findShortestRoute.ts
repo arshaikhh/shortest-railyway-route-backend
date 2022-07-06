edd2553cb4c1a769f43659a92fb77903fd5394ed
Abdul Rehman Shaikh <CORP\2202-001-ars@a-hw0jpshz94r.corp.amazonworkspaces.com>
2022-07-07 00:48:16 +0500

    cleanup

--- utils/findShortestRoute.ts
+++ utils/findShortestRoute.ts
@@ -1,4 +1,5 @@
 import Graph from "node-dijkstra"
+import getDistanceToSubtract from "./getDistanceToSubtract";
 import { Node, Path } from "./interfaces";
 const findShortestRoute = (allTrackData:Node[], tracksWithZeroDistance:Node[], fromTrack:string, toTrack:string) => {
     const route = new Graph()
@@ -8,30 +9,19 @@ const findShortestRoute = (allTrackData:Node[], tracksWithZeroDistance:Node[], f
       const source = track.from_tiploc
       const neighbour = track.to_tiploc
       let distance = track.distance
-      //the zero distance is set to 1 because node-dijkstra doesn't allow 0 cost to be put in. These converted distances -> if found in the shortest route -> are subtracted from the total track in the end by keeping track of a zero counter
+      //the zero distance is set to 1 because node-dijkstra doesn't allow 0 cost to be put in. These converted distances -> if found in the shortest route -> are subtracted from the total track in the end by getting the tracks with zero distance count in the shortest route
       if (distance===0) {
         distance=1
       }
-      
-      
       nodeData[source] ={...nodeData[source], [neighbour]:distance}
       }
     
-   
     for (const nodeSource of Object.keys(nodeData)) {
       route.addNode(nodeSource, nodeData[nodeSource])
     }
     
     const shortestRoute:Path = (route.path(fromTrack,toTrack,{cost:true})) as Path
-    if(shortestRoute!==null) {
-    for(let i=0; i<shortestRoute.path.length-1; i++) {
-      const isZeroDistanceTrackPresent =
-      tracksWithZeroDistance.filter(trackWithZeroDistance=>trackWithZeroDistance.from_tiploc===shortestRoute.path[i] && trackWithZeroDistance.to_tiploc===shortestRoute.path[i+1]).length>0
-      if (isZeroDistanceTrackPresent) {
-        zeroCounter++
-      }
-      }
-    shortestRoute.cost-=zeroCounter //since we converted the zero distances to 1, they have to be subtracted from the total cost
+    const distanceToSubtract = getDistanceToSubtract(tracksWithZeroDistance, shortestRoute)
+    shortestRoute.cost-=distanceToSubtract //since we converted the zero distances to 1, they have to be subtracted from the total cost
     }
-  }
   export default findShortestRoute
